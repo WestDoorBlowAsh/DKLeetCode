5d0d6f4b8b80d9e9c230fc16a9c24aa0758964cc
dengkai <dengkai2333@sina.com>
2019-07-30 16:05:22 +0800

    竞赛两道题

--- src/com/crane/array/Array1_27.java
+++ src/com/crane/array/Array1_27.java
@@ -2,6 +2,15 @@ package com.crane.array;
 
 import java.util.Arrays;
 
+/**
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ *
+ */
+
 public class Array1_27 {
 
     public int removeElement(int[] nums, int val) {
@@ -48,7 +57,7 @@ public class Array1_27 {
 
         int[] nums = {0,1,2,2,3,0,4,2};
         int val = 2;
-        int newLen = removeElement3(nums, val);
+        int newLen = removeElement2(nums, val);
 
         System.out.println(Arrays.toString(nums) + " " + newLen);
     }
